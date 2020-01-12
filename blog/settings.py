deb700abb06580b3a9cd931e68e94dec6b94383b
XB <ssbandjl@163.com>
2020-01-12 21:38:02 +0800

    配置数据库

--- blog/settings.py
+++ blog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '07#4u3t!4*#54s1a091bwdxwiup=#2@6w1i)tsreu6oy=@lb8p'
+SECRET_KEY = '07#4u3t!4*#2020a091bwdxwiup=#2@6w1i)tsreu6oy=@lb8p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,8 +88,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'localhost',
         'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': 'stormsha_blog;',
+        'PASSWORD': 'stormsha_blog2020',
         'NAME': 'blog',
         # 避免映射数据库时出现警告
         'OPTIONS': {
